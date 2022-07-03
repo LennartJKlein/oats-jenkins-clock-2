f8d5a66aa22e4a564d8bd077ed2945f818853c3a
Lennart Klein <lennart.klein@eo.nl>
2022-07-03 17:38:30 +0200

    install g4a

--- src/main.js
+++ src/main.js
@@ -1,9 +1,15 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 import App from "./App.vue";
+import VueGtag from "vue-gtag";
+
 
 const app = createApp(App);
 
 app.use(createPinia());
+
+app.use(VueGtag, {
+  config: { id: "3813844154" },
+});
 
 app.mount("#app");
