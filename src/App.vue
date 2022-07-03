a0a0a0ef93580ffe02da940e9ab4c8471f47a29c
Lennart Klein <lennart.klein@eo.nl>
2022-07-03 11:01:51 +0200

    set up vuejs application

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Clock2 from "./components/Clock2.vue";
+</script>
+
+<template>
+  <main>
+    <Clock2 />
+  </main>
+</template>
+
+<style>
+@import "./assets/base.css";
+
+body {
+  display: flex;
+  place-items: center;
+}
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 2rem;
+}
+</style>
